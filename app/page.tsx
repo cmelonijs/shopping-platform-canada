07cecd0eb60d41477794df99000ccb6e888719ef
cmelonijs <carlomeloni63@gmail.com>
2025-07-01 10:34:58 +0200

    feat: initial commit with styles and prisma packages

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const Homepage = () => {
+  return (
+    <>
+      <Button>Push the button</Button>
+    </>
+  );
+};
+
+export default Homepage;
