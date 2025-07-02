239480b74aa7360f8a74d3ff06eac27cffda2d74
Quoctynoob <tilburgquoc34@gmail.com>
2025-07-02 15:18:17 +0200

    fix/share-components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,5 @@
-import { Button } from "@/components/ui/button";
-import { AlignLeft, EllipsisVertical} from "lucide-react";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
-import Cart  from "@/components/share/cart";
-import Login from "@/components/share/login";
-import Home from "@/components/share/home";
+import Header from "@/components/share/header";
+import Footer from "@/components/share/footer";
 
 export default function RootLayout({
   children,
@@ -12,48 +8,9 @@ export default function RootLayout({
 }>) {
   return (
     <div className="flex flex-col min-h-screen">
-      {/* Header */}
-      <div className="w-full border-b">
-        <div className="container mx-auto px-4 flex justify-between items-center py-4">
-          <div className="flex items-center space-x-4">
-            <Button variant="outline" size ="sm" className="p-4">
-              <AlignLeft className="text-2xl text-black" />
-            </Button>
-            <span className="text-3xl font-bold text-">SimpleShop</span>
-          </div>
-
-          {/* Button */}
-          <div className="items-center space-x-4 hidden md:flex">
-            <Cart/>
-
-            <Login/>
-          </div>
-
-          {/* Mobile Dropdown menu */}
-          <div className="md:hidden">
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="ghost" className="p-4 bg-transparent shadow-none hover:bg-transparent">
-                  <EllipsisVertical className="text-2xl text-black" />
-                </Button>
-              </DropdownMenuTrigger>
-
-              <DropdownMenuContent className="p-2 flex flex-col space-y-2" align="end">
-                  <Home/>
-                  <Cart/>
-                  <Login/>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
-        </div>
-      </div>
-
+      <Header />
       {children}
-
-      {/* Footer */}
-      <footer className="w-full border-t py-4 text-center text-sm">
-        2025 SimpleShop | STORE. All right are reserved.
-      </footer>
+      <Footer />
     </div>
   );
 }
---
+++ components/share/footer.tsx
@@ -0,0 +1,8 @@
+
+export default function Footer() {
+  return (
+    <div className="w-full border-t py-4 text-center text-sm">
+        2025 SimpleShop | STORE. All right are reserved.
+    </div>
+  );
+}
