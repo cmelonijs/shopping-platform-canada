1254f12f2fee1b3cbef725b25efa12a123749e2f
Quoctynoob <tilburgquoc34@gmail.com>
2025-07-02 10:43:36 +0200

    feat/add-footer-header

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,9 @@
 import "./globals.css";
+import { Button } from "@/components/ui/button";
+import { FaRegUser } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+import { RxHamburgerMenu } from "react-icons/rx";
+
 
 export default function RootLayout({
   children,
@@ -6,10 +11,41 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="container">
-      <div>Header</div>
-      <div>Application content</div>
-      <div>Footer</div>
+    <div className="flex flex-col min-h-screen">
+      {/* Header */}
+      <div className="w-full border-b">
+        <div className="container mx-auto px-4 flex justify-between items-center py-4">
+          <div className="flex items-center space-x-4">
+            <Button variant="outline" size ="sm" className="p-4">
+              <RxHamburgerMenu className="text-2xl text-black" />
+            </Button>
+            <span className="text-3xl font-bold text-">SimpleShop</span>
+          </div>
+
+          {/* Button */}
+          <div className="flex items-center space-x-4">
+            <Button variant="outline" size="sm" className="p-4">
+              <FaShoppingCart className="mr-2" />
+              Cart
+            </Button>
+
+            <Button variant="outline" size="sm" className="p-4">
+              <FaRegUser className="mr-2" />
+                Login
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      {/* Main Content */}
+      <main className="flex-grow container mx-auto px-4 py-8">
+        Application content
+      </main>
+
+      {/* Footer */}
+      <footer className="w-full border-t py-4 text-center text-sm">
+        2025 SimpleShop | STORE. All right are reserved.
+      </footer>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import { Button } from "@/components/ui/button";
 
-
 const Homepage = () => {
   return (
     <div className="flex flex-col h-screen items-center justify-center">
-      <Button className="text-xl">Push the button</Button>
-      <h1>Rubik main font testing</h1>
-      <h2 className="font-bebas">Karla font testing</h2>
-    </div>
+     <div>
+       <Button className="text-xl">Push the button</Button>
+       <h1>SimpleShop</h1>
+       <h2 className="font-bebas">ShopSimple</h2>
+  
+     </div>
+   </div>
+
   );
 };
 
