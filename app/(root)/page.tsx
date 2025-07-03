ea379a7df9af683dd3c7a3d6aeb21eab47edd3e9
Quoctynoob <tilburgquoc34@gmail.com>
2025-07-03 14:45:01 +0200

    feat/loading-not-found

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,6 @@
 
-const Homepage = () => {
+export default function Homepage() {
+
   return (
     <div className="flex flex-col h-screen items-center justify-center">
         Homepage content
@@ -7,5 +8,3 @@ const Homepage = () => {
 
   );
 };
-
-export default Homepage;
