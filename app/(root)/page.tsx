65c3c4f839ce1853c9f94ae5ead5edc08d241441
Quoctynoob <tilburgquoc34@gmail.com>
2025-07-03 16:34:26 +0200

    feat/product-card-component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
+import Card1 from "@/components/product/card-1";
 
 export default function Homepage() {
 
   return (
-    <div className="flex flex-col h-screen items-center justify-center">
-        Homepage content
-   </div>
+    <div className="flex flex-col h-screen p-4">
+      <Card1 />
+    </div>
 
   );
 };
---
+++ components/product/card-1.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import product1 from "@/assets/images/sample-products/p1-1.jpg";
+import { Star } from "lucide-react";
+
+export default function Card1() {
+  return (
+    <div className="rounded-lg border-gray-200 border shadow-md p-4 max-w-sm flex flex-col">
+        <Image
+          src={product1}
+          alt="product image"
+          width={280}
+          height={280}
+        />
+        <div className="flex flex-col justify-between flex-1 mt-4">
+          <h2 className="text-lg font-semibold">Polo</h2>
+          <p className="text-gray-600">€140.00</p>
+          <p className="text-gray-500">Made from breathable cotton with a tailored fit—perfect for casual or smart-casual looks.</p>
+        </div>
+        <div className="flex items-center mt-2">
+          <Star className="text-yellow-500 w-4 h-4 fill-yellow-500" />
+          <Star className="text-yellow-500 w-4 h-4 fill-yellow-500" />
+          <Star className="text-yellow-500 w-4 h-4 fill-yellow-500" />
+          <Star className="text-yellow-500 w-4 h-4 fill-yellow-500" />
+          <Star className="text-gray-300 w-4 h-4 " />
+          <div className="ml-2 text-gray-500">4.0</div>
+        </div>
+    </div>
+  );
+}
