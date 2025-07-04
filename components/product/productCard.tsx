d4359897da2e59c39146bdf66881d1cbdfdd31f8
Quoctynoob <tilburgquoc34@gmail.com>
2025-07-04 17:17:49 +0200

    feat:render latest products

--- components/product/productCard.tsx
+++ components/product/productCard.tsx
@@ -8,7 +8,7 @@ export default function ProductCard({ product }: { product: Product }) {
   return (
     <div className="rounded-lg border-gray-200 border shadow-md p-4 max-w-sm flex flex-col items-start">
       <Image
-        src={product.image}
+        src={product.images[0]}
         alt="product image"
         width={280}
         height={280}
@@ -16,7 +16,7 @@ export default function ProductCard({ product }: { product: Product }) {
       />
       <div className="flex flex-col justify-between flex-1 mt-4">
         <h2 className="text-lg font-semibold">{product.name}</h2>
-        <p className="text-gray-600">€{product.price.toFixed(2)}</p>
+        <p className="text-gray-600">€{product.price}</p>
         <p className="text-gray-500">{product.description}</p>
       </div>
       <div className="flex items-center mt-2">
--- components/product/productList.tsx
+++ components/product/productList.tsx
@@ -1,13 +1,19 @@
 import ProductCard from "./productCard";
+import { getLatestProducts } from "@/lib/actions/products.actions";
 
-export default function ProductList() {
+export default async function ProductList() {
+  const products = await getLatestProducts();
+  console.log('====================',products);
+
   return (
     <div>
       <h1 className="text-2xl font-bold mb-4">Newest Arrivals</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">
-        {[].map((product, idx) => (
+        { products.map((product, idx) => (
+          
           <ProductCard key={idx} product={product} />
         ))}
+        
       </div>
     </div>
   );
