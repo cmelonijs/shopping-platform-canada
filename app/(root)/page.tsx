9ea28c9df62c8b1c8f78f72b923d3576446aff83
Quoctynoob <tilburgquoc34@gmail.com>
2025-07-04 10:12:32 +0200

    feat/product-list

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,10 @@
-import ProductCard from "@/components/product/card-1";
+import ProductList from "@/components/product/productList";
 
 export default function Homepage() {
 
   return (
-    <div className="flex flex-col h-screen p-4">
-      <ProductCard />
+    <div className="flex flex-col h-full p-4 container w-full mx-auto">
+      <ProductList />
     </div>
 
   );
