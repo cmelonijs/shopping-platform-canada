64d942d0228585b9f2e033a440016bc81632359d
Quoctynoob <tilburgquoc34@gmail.com>
2025-07-04 17:29:53 +0200

    feat:render latest products

--- components/product/productList.tsx
+++ components/product/productList.tsx
@@ -3,17 +3,15 @@ import { getLatestProducts } from "@/lib/actions/products.actions";
 
 export default async function ProductList() {
   const products = await getLatestProducts();
-  console.log('====================',products);
+  console.log(products);
 
   return (
     <div>
       <h1 className="text-2xl font-bold mb-4">Newest Arrivals</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">
         { products.map((product, idx) => (
-          
           <ProductCard key={idx} product={product} />
         ))}
-        
       </div>
     </div>
   );
