6859b090dfb446450f715aab9ffbd160edf051d7
cmelonijs <carlomeloni63@gmail.com>
2025-07-04 11:39:55 +0200

    feat: add zod validation

--- components/product/productCard.tsx
+++ components/product/productCard.tsx
@@ -1,40 +1,32 @@
 import Image from "next/image";
 import { Star } from "lucide-react";
-import type { StaticImageData } from "next/image";
 
-type Product = {
-  id: number;
-  name: string;
-  price: number;
-  description: string;
-  image: string | StaticImageData;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Product = any;
 
-}
-
-export default function ProductCard({product}: { product: Product } ) {
-
+export default function ProductCard({ product }: { product: Product }) {
   return (
     <div className="rounded-lg border-gray-200 border shadow-md p-4 max-w-sm flex flex-col items-start">
-        <Image
-          src={product.image}
-          alt="product image"
-          width={280}
-          height={280}
-          className="self-center"
-        />
-        <div className="flex flex-col justify-between flex-1 mt-4">
-          <h2 className="text-lg font-semibold">{product.name}</h2>
-          <p className="text-gray-600">€{product.price.toFixed(2)}</p>
-          <p className="text-gray-500">{product.description}</p>
-        </div>
-        <div className="flex items-center mt-2">
-          <Star className="text-yellow-500 w-4 h-4 fill-yellow-500" />
-          <Star className="text-yellow-500 w-4 h-4 fill-yellow-500" />
-          <Star className="text-yellow-500 w-4 h-4 fill-yellow-500" />
-          <Star className="text-yellow-500 w-4 h-4 fill-yellow-500" />
-          <Star className="text-yellow-500 w-4 h-4 fill-yellow-500" />
-          <div className="ml-2 text-gray-500">5.0</div>
-        </div>
+      <Image
+        src={product.image}
+        alt="product image"
+        width={280}
+        height={280}
+        className="self-center"
+      />
+      <div className="flex flex-col justify-between flex-1 mt-4">
+        <h2 className="text-lg font-semibold">{product.name}</h2>
+        <p className="text-gray-600">€{product.price.toFixed(2)}</p>
+        <p className="text-gray-500">{product.description}</p>
+      </div>
+      <div className="flex items-center mt-2">
+        <Star className="text-yellow-500 w-4 h-4 fill-yellow-500" />
+        <Star className="text-yellow-500 w-4 h-4 fill-yellow-500" />
+        <Star className="text-yellow-500 w-4 h-4 fill-yellow-500" />
+        <Star className="text-yellow-500 w-4 h-4 fill-yellow-500" />
+        <Star className="text-yellow-500 w-4 h-4 fill-yellow-500" />
+        <div className="ml-2 text-gray-500">5.0</div>
+      </div>
     </div>
   );
 }
--- components/product/productList.tsx
+++ components/product/productList.tsx
@@ -1,65 +1,14 @@
 import ProductCard from "./productCard";
-import product1 from "@/assets/images/sample-products/p1-1.jpg";
-import product2 from "@/assets/images/sample-products/p2-1.jpg";
-import product3 from "@/assets/images/sample-products/p3-1.jpg";
-import product4 from "@/assets/images/sample-products/p4-1.jpg";
-import product5 from "@/assets/images/sample-products/p5-1.jpg";
-import product6 from "@/assets/images/sample-products/p6-1.jpg";
-
-const products = [
-    {
-        id: 1,
-        name: "Blue Dress Shirt",
-        price: 140.00,
-        description: "Made from breathable cotton with a tailored fit—perfect for casual or smart-casual looks.",
-        image: product1,
-    },
-    {
-        id: 2,
-        name: "Stripe Brown Dress Shirt",
-        price: 140.00,
-        description: "Timeless plaid style, great for layering or wearing solo—crafted for comfort and versatility.",
-        image: product2,
-    },
-    {
-        id: 3,
-        name: "Beige Check Dress Shirt",
-        price: 120.00,
-        description: "Understated check pattern with a classic cut—easy to dress up or down.",
-        image: product3,
-    },
-    {
-        id: 4,
-        name: "Olive Green Dress Shirt",
-        price: 180.00,
-        description: "Military-inspired style with utility pockets—adds an edge to any outfit.",
-        image: product4,
-    },
-    {
-        id: 5,
-        name: "Blue Striped Dress Shirt",
-        price: 200.00,
-        description: "A modern take on a classic pinstripe—sharp and versatile for office or weekend wear.",
-        image: product5,
-    },
-    {
-        id: 6,
-        name: "Pink Hoodie",
-        price: 90.00,
-        description: "Soft, cozy, and casual—this pink hoodie is perfect for relaxed days.",
-        image: product6,
-    },
-]
 
 export default function ProductList() {
-    return (
-        <div>
-            <h1 className="text-2xl font-bold mb-4">Newest Arrivals</h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">
-                {products.map(product => (
-                    <ProductCard key={product.id} product={product} />
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Newest Arrivals</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">
+        {[].map((product, idx) => (
+          <ProductCard key={idx} product={product} />
+        ))}
+      </div>
+    </div>
+  );
 }
