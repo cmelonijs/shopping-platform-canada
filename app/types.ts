0b46c97117178e2daf97599c221221f4073d66c2
cmelonijs <carlomeloni63@gmail.com>
2025-07-07 14:58:51 +0200

    feat: serverless configuration and types fixes

--- app/types.ts
+++
@@ -1,5 +0,0 @@
-import z from "zod";
-import { insertProductSchema } from "@/lib/validator";
-
-
-export type Product = z.infer<typeof insertProductSchema>;
--- components/product/productList.tsx
+++ components/product/productList.tsx
@@ -1,16 +1,17 @@
+import { Product } from "@/types";
 import ProductCard from "./productCard";
 import { getLatestProducts } from "@/lib/actions/products.actions";
 
 export default async function ProductList() {
-  const products = await getLatestProducts();
+  const products: Product[] = await getLatestProducts();
   console.log(products);
 
   return (
     <div>
       <h1 className="text-2xl font-bold mb-4">Newest Arrivals</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">
-        { products.map((product, idx) => (
-          <ProductCard key={idx} product={{...product, price: product.price.toString(),}}/>
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
         ))}
       </div>
     </div>
