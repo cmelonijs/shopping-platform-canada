03899a698cfd02e62181713435660f019e492c57
cmelonijs <carlo.meloni@abinsula.com>
2025-07-10 10:06:11 +0200

    feat: minimal refactor

--- lib/actions/auth.actions.ts
+++ lib/actions/auth.actions.ts
@@ -1,43 +1,38 @@
-"use server";
-
+"user server";
 import { signIn, signOut } from "@/auth";
-import { redirect } from "next/navigation";
-import { AuthError } from "next-auth";
+import { isRedirectError } from "next/dist/client/components/redirect-error";
+import { signInFormSchema } from "../validator";
 
-export async function signInAction(formData: FormData) {
- const email = formData.get("email");
- const password = formData.get("password");
+// sign in the user with credentials
+export async function signInWithCredentials(
+  prevState: unknown,
+  formData: FormData
+) {
+  try {
+    const user = signInFormSchema.parse({
+      email: formData.get("email"),
+      password: formData.get("password"),
+    });
 
+    await signIn("credentials", user);
 
- if (typeof email !== "string" || typeof password !== "string") {
-   throw new Error("Email and password are required");
- }
+    return {
+      success: true,
+      message: "Signed in successfully!",
+    };
+  } catch (err) {
+    if (isRedirectError(err)) {
+      throw err;
+    }
 
-
- const res = await signIn("credentials", {
-   redirect: false,
-   email,
-   password,
- });
-
-
- if (res?.error) {
-   throw new AuthError(res.error);
- }
-
-
- redirect("/");
+    return {
+      success: false,
+      message: "Invalid email or password",
+    };
+  }
 }
 
-export async function signOutAction() {
- const res = await signOut({
-   redirect: false,
- });
-
- if (res?.error) {
-   throw new AuthError(res.error);
- }
-
- redirect("/");
+// sign user out
+export async function signOutUser() {
+  await signOut();
 }
-
