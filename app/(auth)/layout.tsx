6cba543f992706697e4ac86bb4951bdf105741b9
Quoctynoob <tilburgquoc34@gmail.com>
2025-07-10 10:55:01 +0200

    feat/auth-layout

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/share/header";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex flex-col min-h-screen">
+        {children}
+    </div>
+  );
+}
