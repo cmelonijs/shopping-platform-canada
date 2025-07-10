94b42e7986c52377242310fe6976dd82424296bf
Quoctynoob <tilburgquoc34@gmail.com>
2025-07-10 11:26:47 +0200

    fix/png-jpg

--- components/product/ProductPrice.tsx
+++ components/product/ProductPrice.tsx
@@ -13,7 +13,7 @@ const ProductPrice = ({
 
   return (
     <p className={cn("text-2xl", className)}>
-      <span className="text-xs align-super">€</span>
+      <span className="text-xs align-super">$</span>
       {intValue}
       <span className="text-xs align-super">{floatValue}</span>
     </p>
