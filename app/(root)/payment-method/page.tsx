5ff4475469657e830cb3e06176c72da31f393267
Pablo0007-ita <paolo.fenu@gmail.com>
2025-08-26 13:03:15 +0200

    feat: payment-method-page

---
+++ app/(root)/payment-method/page.tsx
@@ -0,0 +1,11 @@
+
+export default function PaymentMethodPage() {
+  return (
+<div className="flex justify-center h-screen">
+  <div className="max-w-md w-full space-y-4 p-6 text-center">
+    <h1 className="text-[2.2rem] font-bold mt-4">Payment Method</h1>
+  </div>
+</div>
+
+  );
+}
