59643fdbd4fe53beca0fd59fbe6ea823dd6f8134
Pablo0007-ita <paolo.fenu@gmail.com>
2025-08-27 14:12:55 +0200

    feat: implement BreadcrumbBoard component and update pages to use it

--- app/(root)/payment-method/page.tsx
+++ app/(root)/payment-method/page.tsx
@@ -1,11 +1,14 @@
+import BreadcrumbBoard from "@/components/share/breadcrumbBoard";
+
 
 export default function PaymentMethodPage() {
     return (
-        <div className="flex justify-center h-screen">
-            <div className="max-w-md w-full space-y-4 p-6 text-center">
-                <h1 className="text-[2.2rem] font-bold mt-4">Payment Method</h1>
+        <>
+            <BreadcrumbBoard step='payment' />
+            <div className='max-w-2xl mx-auto p-6 h-screen'>
+                <h1 className="text-3xl font-bold mb-6">Payment Method</h1>
+                <p className="text-sm text-muted-foreground">Please select the payment method</p>
             </div>
-        </div>
-
+        </>
     );
 }
