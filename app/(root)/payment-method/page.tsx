e8a11d7500644f82bca9452700f6a29bd89f666c
Quoctynoob <tilburgquoc34@gmail.com>
2025-09-01 11:53:40 +0200

    fix:added-toast-feedback

--- app/(root)/payment-method/page.tsx
+++ app/(root)/payment-method/page.tsx
@@ -12,7 +12,7 @@ const PaymentMethodPage = async (props: {
     <>
       <BreadcrumbBoard step='payment method' />
       <div className='max-w-2xl mx-auto p-6 h-screen'>
-        <h1 className="text-3xl font-bold mb-6 ">Payment Method testing</h1>
+        <h1 className="text-3xl font-bold mb-6 ">Payment Method</h1>
         <PaymentForm defaultValues={existingPaymentMethod as PaymentMethod} />
       </div>
     </>
