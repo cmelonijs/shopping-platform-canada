d03ad7bba47005c2730d16fc3843a73319a3ebeb
Pablo0007-ita <paolo.fenu@gmail.com>
2025-09-08 14:23:56 +0200

    fixed layout for user

--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -1,8 +1,5 @@
 
 import UserHeader from "./userHeader";
-import { SessionProvider } from "next-auth/react";
-import { Toaster } from "@/components/ui/sonner";
-
 
 export default function UserLayout({
   children,
@@ -12,11 +9,8 @@ export default function UserLayout({
 
   return (
     <div className="flex flex-col min-h-screen">
-
-        <UserHeader />
-        {children}
-        
-     
+      <UserHeader />
+      {children}
     </div>
   );
 }
